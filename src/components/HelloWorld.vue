ee5767bce5f65298d69fcb0150e32b16911e5301
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-05-15 21:05:00 +0200

    Display hardcoded actions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="action in actions" v-bind:key="action.time">{{ action.type }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  data: () => ({
+    actions: [
+      { type: "lock", time: 1557945949433, status: "COMPLETE" },
+      { type: "unlock", time: 1557945991480, status: "PENDING" }
+    ]
+  }),
   props: {
     msg: String
   }
@@ -17,5 +26,9 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
+}
+
+li {
+  list-style: none;
 }
 </style>
