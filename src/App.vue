b73194e2be650269428646563e5aef50b627ca07
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-05-16 21:13:36 +0200

    Style actions and add title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div class="app">
+    <h2>Actions</h2>
     <ul>
       <li v-for="action in actions" v-bind:key="action.time">{{ action.type }}</li>
     </ul>
@@ -19,11 +20,31 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.app {
+  padding: 1rem;
+}
+
+ul {
+  padding: 0;
+}
+
+li:first-child {
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
 }
 
 li {
   list-style: none;
+  position: relative;
+  display: block;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: -1px;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+}
+
+li:last-child {
+  border-bottom-right-radius: 0.25rem;
+  border-bottom-left-radius: 0.25rem;
 }
 </style>
