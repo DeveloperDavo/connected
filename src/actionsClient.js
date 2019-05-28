5e654f92c89b62208eb83b81fb07180b810da8f4
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-05-28 21:30:46 +0200

    Update hardcoded data

--- src/actionsClient.js
+++ src/actionsClient.js
@@ -1,6 +1,8 @@
 export default function getActions() {
   return [
-    { type: "lock", time: 1557945949433, status: "COMPLETE" },
-    { type: "unlock", time: 1557945991480, status: "PENDING" }
+    { type: "UNLOCK", time: 1558985702333, status: "PENDING" },
+    { type: "LOCK", time: 1558985698238, status: "FAILED" },
+    { type: "UNLOCK", time: 1557945991480, status: "COMPLETE" },
+    { type: "LOCK", time: 1557945949433, status: "COMPLETE" }
   ];
 }
